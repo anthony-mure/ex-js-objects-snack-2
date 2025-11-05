27611e19ab6d45e3b5fdc07169fcc152ab8d9490
anthony-murè <tony.mure99@gmail.com>
2025-11-05 16:02:21 +0100

    [add]snack-3

--- main.js
+++ main.js
@@ -38,3 +38,27 @@ console.log(secondBurger.ingredients[0]); // ?
 // 1-(Salad)
 // 2-Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
 // 2-(2)
+
+//Code Question 3
+
+const hamburger = {
+  name: "Cheese Burger",
+  weight: 250,
+  maker: {
+    name: "Anonymous Chef",
+    restaurant: {
+      name: "Hyur's Burgers",
+      address: "Main Street, 123",
+      isOpen: true,
+    },
+    age: 29
+  }
+};
+
+const secondBurger = structuredClone(hamburger);
+const thirdBurger = structuredClone(hamburger);
+
+//domande:
+
+// 1-Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
+// 1(9)
